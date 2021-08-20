956f2c14fe676577470525674eb1e14c721aeaa2
Abdullahoo433 <89166029+Abdullahoo433@users.noreply.github.com>
2021-08-20 17:40:07 +0500

    Add files via upload

---
+++ Area.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	cout<<"THIS PROGRAM WILL DISPLAY AREA OF RECTANGLE\n";
+	double length,width,area;
+	
+	cout<<"Enter length\n";
+	cin>>length;
+	cout<<"Enter width\n";
+	cin>>width;
+	area=length*width;
+	cout<<"Area is " <<area;
+}
